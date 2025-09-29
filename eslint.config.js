3faf39802f32b9d53b287aa029bf41337d2c5599
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-29 17:07:49 +0800

    ESLint: Added config for stacking imported modules

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,28 @@ export default defineConfig([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
