9a859a2c76ffa9371f2e2132ed504cf36e1b658b
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-30 17:29:12 +0800

    Header: Added basic login/register buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,13 @@ const Header = () => {
       <div className="align-element flex sm:justify-end">
         {/* USER */}
         {/* LINKS */}
-        <div className="flex gap-x-y justify-center items-center">
-          <Link to="/login" className="link link-hover text-cs sm:text-sm">
+        <div className="flex gap-x-y justify-center items-center gap-x-4 ">
+          <button className='btn btn-primary h-[33px]'><Link to="/login" className="link link-hover text-cs sm:text-sm">
             Login
-          </Link>
+          </Link></button>
+          <button className='btn btn-primary h-[33px]'><Link to="/login" className="link link-hover text-cs sm:text-sm">
+            Sign Up
+          </Link></button>
         </div>
       </div>
     </header>
