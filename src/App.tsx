46ebea27faf3a575d9fccda2343228494bd992e7
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-30 13:38:30 +0800

    Dependency: Added linter configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import {DashboardAdmin, DashboardUser, Login, Receipts, Signup, Stocktrading, Traders, Wallet} from './pages/index';
+import {
+  DashboardAdmin,
+  DashboardUser,
+  Login,
+  Receipts,
+  Signup,
+  Stocktrading,
+  Traders,
+  Wallet,
+} from './pages/index';
 
 function App() {
   return (
     <>
       <p> Hello World! </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
