08675d4bee3f7c317885398ed419726c132a797e
Gramnaster <jocvillalon@yahoo.com.ph>
2025-09-30 14:15:24 +0800

    Structure: Adjusted routes to basic user needs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {
+  Companies,
   DashboardAdmin,
   DashboardUser,
   Home,
@@ -8,15 +9,58 @@ import {
   Stocktrading,
   Traders,
   Wallet,
-  Error
+  Error,
+  NotFound
 } from './pages/index';
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path:'/',
+    path: '/',
     element: <Home />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <DashboardUser />,
+      },
+      {
+        path: 'companies',
+        element: <Companies />,
+      },
+      {
+        path: 'wallet/:id',
+        element: <Wallet />,
+      },
+      {
+        path: 'stocktrading/:id',
+        element: <Stocktrading />,
+      },
+      {
+        path: 'transactions/:id',
+        element: <Receipts />,
+      },
+      {
+        path: 'admin',
+        element: <DashboardAdmin />,
+        children: [
+          {
+            index: true,
+            element: <Traders />
+          }
+        ]
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
     errorElement: <Error />,
   },
 ]);
